f11fdb75d3207134017c49a85689b9167b189c2f
Matija Katanec <mkatanec@croz.net>
2020-06-09 18:49:12 +0200

    added Field and some constants

---
+++ src/constants/colors.js
@@ -0,0 +1,18 @@
+export default {
+  BOARD_COLORS: {
+    BROWN: "brown",
+    LIGHT_BLUE: "lightBlue",
+    PINK: "pink",
+    ORANGE: "orange",
+    RED: "red",
+    YELLOW: "yellow",
+    GREEN: "green",
+    BLUE: "blue",
+  },
+  PLAYER_COLORS: {
+    RED: "red",
+    YELLOW: "yellow",
+    GREEN: "green",
+    BLUE: "blue",
+  }
+}
---
+++ src/constants/orientation.js
@@ -0,0 +1,6 @@
+export default {
+  TOP: "180",
+  BOTTOM: "0",
+  LEFT: "90",
+  RIGHT: "270",
+}
