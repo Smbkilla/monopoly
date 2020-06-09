3b6203cf147dcc7767a4763fa9c9ec13a7e1890f
Matija Katanec <mkatanec@croz.net>
2020-06-09 17:17:47 +0200

    ono malo

--- src/App.js
+++ src/App.js
@@ -1,24 +1,86 @@
 import React from 'react';
-import logo from './logo.svg';
+
+import Card from "@material-ui/core/Card";
+import Grid from "@material-ui/core/Grid";
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Grid container justify="center">
+        <Grid item direction="row" alignItems="center" xs={12}>
+          <Grid item xs={1}>
+            <Card>
+              Parking
+            </Card>
+          </Grid>
+          <Grid item xs={1}>
+            <Card>
+              Polje 1
+            </Card>
+          </Grid>
+          <Grid item xs={1}>
+            <Card>
+              Polje 2
+            </Card>
+          </Grid>
+          <Grid item xs={1}>
+            <Card>
+              Polje 3
+            </Card>
+          </Grid>
+          <Grid item xs={1}>
+            <Card>
+              Polje 4
+            </Card>
+          </Grid>
+          <Grid item xs={1}>
+            <Card>
+              Polje 5
+            </Card>
+          </Grid>
+          <Grid item xs={1}>
+            <Card>
+              Polje 6
+            </Card>
+          </Grid>
+          <Grid item xs={1}>
+            <Card>
+              Polje 7
+            </Card>
+          </Grid>
+          <Grid item xs={1}>
+            <Card>
+              Polje 8
+            </Card>
+          </Grid>
+          <Grid item xs={1}>
+            <Card>
+              Polje 9
+            </Card>
+          </Grid>
+          <Grid item xs={2}>
+            <Card>
+              Go to jail
+            </Card>
+          </Grid>
+        </Grid>
+        <Grid item direction="row" xs={12}>
+          <Grid item xs={1}>
+            <Card>
+              Polje 21
+            </Card>
+          </Grid>
+          <Grid item xs={10}>
+          </Grid>
+          <Grid item xs={1}>
+            <Card>
+              Polje 22
+            </Card>
+          </Grid>
+        </Grid>
+      </Grid>
     </div>
   );
 }
